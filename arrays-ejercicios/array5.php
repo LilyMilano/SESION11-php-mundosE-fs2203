0783e03de8f751b697af8b2a1cfdfd5e7f9743cd
Liliana Milano <milanoliliana129@gmail.com>
2022-11-18 03:45:26 -0400

    Add HTML file with embedded PHP code. Final file

--- arrays-ejercicios/array5.php
+++ arrays-ejercicios/array5.php
@@ -4,9 +4,53 @@ Elaborar un programa para imprimir una tabla con los cuadrados y los cubos de lo
 20 primeros números enteros.
 a. Recordar que para recorrer un array tenemos el for y foreach y echo para
 mostrar el elemento.
+*TODO: solución con ciclo while y código php embebido. Consultar array =)
 */
 
-
-
+$num=1;
 
 ?>
+
+<!--////////////////////////////////////////////////////////////////////
+                    TABLA HTML CON CÓDIGO PHP EMBEBIDO
+//////////////////////////////////////////////////////////////////////--->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de Cuadrados y Cubos</title>
+</head>
+
+<body>
+    <div class="table-responsive">
+        <table class="table table-primary">
+            <thead>
+                <tr>
+                    <th>Número</th>
+                    <th>Cuadrado</th>
+                    <th>Cubo</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php while($num>0 && $num<=20) { ?>
+
+                <tr>
+                    <td><?php echo $num; ?></td>
+                    <td><?php echo pow($num,2); ?></td>
+                    <td><?php echo pow($num,3); ?></td>
+                </tr>
+                <?php $num++; ?>
+
+                <?php } ?>
+
+            </tbody>
+        </table>
+
+    </div>
+</body>
+
+</html>
